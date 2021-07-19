197e88bd95d3610f2aaa0c39a2ee4e40a02cfe0a
Maarten L. Hekkelman <maarten@hekkelman.com>
2021-07-19 14:08:49 +0200

    reimplement fisher exact code

--- src/fisher.hpp
+++ src/fisher.hpp
@@ -8,5 +8,22 @@ enum class FisherAlternative {
 	Left, Right, TwoSided
 };
 
-double fisherTest2x2(long v[2][2], FisherAlternative alternative = FisherAlternative::TwoSided);
+class FishersExactTest
+{
+  public:
+	FishersExactTest(long v[2][2], FisherAlternative alternative = FisherAlternative::TwoSided);
+
+	double pvalue() const			{ return m_pvalue; }
+	double oddsRatio() const		{ return m_oddsRatio; }
+
+  private:
+	double m_pvalue;
+	double m_oddsRatio;
+};
+
+double fisherTest2x2(long v[2][2], FisherAlternative alternative = FisherAlternative::TwoSided)
+{
+	return FishersExactTest(v, alternative).pvalue();
+}
+
 std::vector<double> adjustFDR_BH(const std::vector<double>& p);
